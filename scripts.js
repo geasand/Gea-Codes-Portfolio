8720b3c2eadbfc0ba5051e96bfc6bc3ffe02a571
Gea <sandmop39@gmail.com>
2023-02-23 01:27:26 -0400

    First commit

---
+++ scripts.js
@@ -0,0 +1,21 @@
+// Getting the Go to Top button:
+let scrollBtn = document.getElementById("goTop")
+
+
+// Showing button when the user scrolls down 100px from the top of the document
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+
+    if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+        scrollBtn.style.display = "block";
+    } else {
+        scrollBtn.style.display = "none";
+    }
+}
+
+// Scroll to the top of the document when the button is clicked
+function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
