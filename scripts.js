348644bd715dd5819d7c3b7452150644db898049
Gea <sandmop39@gmail.com>
2024-06-15 12:16:32 -0400

    Dark mode finished

--- scripts.js
+++ scripts.js
@@ -22,24 +22,35 @@ function topFunction() {
 
 // Dark/Light Mode Switch
 const btnSwitch = document.querySelector('#switch');
+const sections = document.querySelectorAll('.profile, .timeline, .outer, .card2, .card2-footer-container, .title, .icon');
 
+// Switching each section
 btnSwitch.addEventListener('click', () => {
-    document.body.classList.toggle('dark');
     btnSwitch.classList.toggle('active');
+    document.body.classList.toggle('dark');
+    sections.forEach(sections => {
+        sections.classList.toggle('dark');
+    });
 
     // Save Dark/Light Mode State in LocalStorage
     if (document.body.classList.contains('dark')) {
-        localStorage.setItem('dark-mode', 'true');
+    localStorage.setItem('dark-mode', 'true');
     } else {
-        localStorage.setItem('dark-mode', 'false');
+    localStorage.setItem('dark-mode', 'false');
     }
 });
 
 //Current Dark/Light Mode State
 if (localStorage.getItem('dark-mode') === 'true') {
     document.body.classList.add('dark');
+    sections.forEach(sections => {
+        sections.classList.add('dark');
+    });
     btnSwitch.classList.add('active');
 } else {
     document.body.classList.remove('dark');
+    sections.forEach(sections => {
+        sections.classList.remove('dark');
+    });
     btnSwitch.classList.remove('active');
 }
